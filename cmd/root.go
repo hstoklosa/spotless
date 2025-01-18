a919741e05d86ff35e013e246b5afb046ad66032
hstoklosa <hubert.stoklosa23@gmail.com>
2025-01-18 02:38:54 +0000

    modify arguments and flags for root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,43 +1,38 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "spotless",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-		   examples and usage of using your application. For example:
 
-		   Cobra is a CLI library for Go that empowers applications.
-		   This application is a tool to generate the needed files
-		   to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+var rootCmd = &cobra.Command{
+    Use:   "spotless [directory]",
+    Short: "Spotless keeps your directories clean and organized",
+    Long: `Spotless is a CLI tool for organizing your files effectively.
+Use Spotless to organize a directory by letting it place files into separate 
+folders based on their name, extension...`,
+    Args: cobra.MaximumNArgs(1),
+    Run: func(cmd *cobra.Command, args []string) {
+        toggle, _ := cmd.Flags().GetBool("toggle")
+        fmt.Println(toggle, args)
+    },
 }
 
-// Adds all child commands to the root command 
-// and sets flags appropriately.
+/** Adds all child commands to the root command 
+ *  and sets flags appropriately.
+ */
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
 
 func init() {
-	// Flags and configuration settings.
-	
-	// Persistent flags (global in the entire app)
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.spotless.yaml)")
-
 	// Local flags (run when action is called directly)
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("name", "n", false, "group based on the starting names")
+	rootCmd.Flags().BoolP("ext", "e", false, "group based on the file extensions")
+	rootCmd.Flags().BoolP("date", "d", false, "group based on the creation dates")
 }
-
-
--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "github.com/hstoklosa/spotless/cmd"
 
+/** Commands represent actions, Args are things and Flags are modifiers for those actions.
+ *  APPNAME VERB NOUN --ADJECTIVE or APPNAME COMMAND ARG --FLAG.
+ */
+
 func main() {
 	cmd.Execute()
 }
